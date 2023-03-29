da608388e05e63e466ed736011ff0e7cc04e65b1
nidha <thamanna96nt@gmail.com>
2023-03-29 12:57:25 +0530

    ecommerce

---
+++ customer/urls.py
@@ -0,0 +1,6 @@
+from.import views
+from django.urls import path
+urlpatterns=[
+    path('c_home',views.c_home)
+    
+]
---
+++ seller/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def c_home(request):
+    return render(request,'seller/seller.html')
